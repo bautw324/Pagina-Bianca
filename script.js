985abd15c27673771f66eb39ce64e05dac9dbb38
bautw324 <112228409+bautw324@users.noreply.github.com>
2025-10-12 18:54:24 -0300

    hice de todo

--- script.js
+++ script.js
@@ -1,62 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const header = document.querySelector('header');
-    const mainContent = document.querySelector('.main-content');
-    const hamburgerBtn = document.querySelector('.hamburger-btn');
-    const mobileMenu = document.querySelector('#menu');
-
-    function ajustarPadding() {
-        const alturaHeader = header.offsetHeight;
-        mainContent.style.paddingTop = alturaHeader+10+'px';
-    }
-    ajustarPadding();
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const menu = document.getElementById('menu');
 
     hamburgerBtn.addEventListener('click', function() {
-        mobileMenu.classList.toggle('activo');
-        ajustarPadding();
+        menu.classList.toggle('activo');
     });
+
     document.addEventListener('click', function(event) {
-        if (!hamburgerBtn.contains(event.target) && !mobileMenu.contains(event.target)) {
-            mobileMenu.classList.remove('activo');
-        }
-    });
-    window.addEventListener('resize', ajustarPadding);
-
-    const agregarBtn = document.getElementById('agregarBtn');
-    const lista = document.getElementById('lista');
-    const texto = document.getElementById('texto');
-
-    agregarBtn.addEventListener('click', function(){
-        if(texto.value.trim() !== ''){
-            const inciso = document.createElement("li");
-            inciso.innerText = texto.value;
-            lista.appendChild(inciso);
-            texto.value = '';
-        }
-    });
-
-    const buscador = document.getElementById('buscador');
-    const cajas = document.querySelectorAll('.caja');
-    buscador.addEventListener('input', function(){
-        const filtro = buscador.value.toLowerCase();
-        let encontrado = false;
-        cajas.forEach(function(caja){
-            const titulo = caja.querySelector('h3').innerText.toLowerCase();
-            if(titulo.includes(filtro)){
-                caja.style.display = '';
-                encontrado = true;
-            } else {
-                caja.style.display = 'none';
-            }  
-        });
-        if (filtro === '') {
-            document.getElementById('resultado').style.display = 'none';
-        } else if (!encontrado) {
-            document.getElementById('resultado').style.display = 'block';
-            document.getElementById('resultado').innerText = `No se encontraron resultados para: "${filtro}"`;
-        } else {
-            encontrado = true;
-            document.getElementById('resultado').style.display = 'block';
-            document.getElementById('resultado').innerText = `Resultados para: "${filtro}"`;
+        if (!hamburgerBtn.contains(event.target) && !menu.contains(event.target)) {
+            menu.classList.remove('activo');
         }
     });
 });
