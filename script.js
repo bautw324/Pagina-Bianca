959349d3a207f97a806298d6f6ca941d933be231
bautw324 <bautwzz@gmail.com>
2025-10-14 10:57:40 -0300

    Agregué un efecto Blur

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const hamburgerBtn = document.getElementById('hamburger-btn');
     const menu = document.getElementById('menu');
+    const menuOverlay = document.querySelector('.menu-overlay');
     const botonBusqueda = document.getElementById("botonBusqueda");
     const barraBusqueda = document.getElementById("barraBusqueda-desplegable");
     const header = document.getElementById("header");
@@ -8,11 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
     hamburgerBtn.addEventListener('click', function(event) {
         event.preventDefault();
         menu.classList.toggle('activo');
+        menuOverlay.classList.toggle('activo');
     });
 
     document.addEventListener('click', function(event) {
         if (!hamburgerBtn.contains(event.target) && !menu.contains(event.target)) {
             menu.classList.remove('activo');
+            menuOverlay.classList.remove('activo');
         }
     });
 
