47c53eb19e5b17375708021c010ebee116f4799e
bautw324 <bautwzz@gmail.com>
2025-10-14 19:11:06 -0300

    Mejoré el blur

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuOverlay = document.querySelector('.menu-overlay');
     const botonBusqueda = document.getElementById("botonBusqueda");
     const barraBusqueda = document.getElementById("barraBusqueda-desplegable");
-    const header = document.getElementById("header");
 
     hamburgerBtn.addEventListener('click', function(event) {
         event.preventDefault();
         menu.classList.toggle('activo');
-        menuOverlay.classList.toggle('activo');
+        barraBusqueda.classList.remove('activo');
+        barraBusqueda.value = '';
+        if (!menuOverlay.classList.contains('activo')) {
+            menuOverlay.classList.toggle('activo');
+        }
     });
 
     botonBusqueda.addEventListener('click', function(event) {
         event.preventDefault();
-
-        const headerHeight = header.offsetHeight;
-        barraBusqueda.style.top = headerHeight + 'px';
         barraBusqueda.classList.toggle('activo');
-        menuOverlay.classList.toggle('activo');
-
+        menu.classList.remove('activo');
+        if (!menuOverlay.classList.contains('activo')) {
+            menuOverlay.classList.toggle('activo');
+        }
         if (barraBusqueda.classList.contains('activo')) {
             barraBusqueda.focus();
         }
